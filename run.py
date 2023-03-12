5585729385f2bbfe4ad0a2e26a4f539e4f2db259
FarehaSi <fareha.s@live.com>
2023-03-12 15:37:43 +0000

    Specify key functions

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def print_board():
+    pass
+
+def create_ships():
+    pass
+
+def get_ship_location():
+    pass
+
+def count_hits():
+    pass
