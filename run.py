9a57999dc8935d34c319b0483d97ab4f828a0f46
FarehaSi <fareha.s@live.com>
2023-03-16 09:18:36 +0000

    Edit code to display user and computer choice in a number instead of coordinates. Add message indicating whether the computer has made a hit or missed

--- run.py
+++ run.py
@@ -24,7 +24,7 @@ class Board:
 
     def score(self,row,col):
         """
-        manipulate scores and marks the hit spot
+        Manipulate scores and marks the hit spot
         """
         temp = self.score_val
         if self.board_pattern[row][col] == 'X':
@@ -39,7 +39,7 @@ class Board:
 
     def toList(self):
         """
-        return the board for printing
+        Return the board for printing
         """
         return self.print_board
 
@@ -52,12 +52,22 @@ class Board:
         self.count = count
 
     def find_cell(self,cell):
+        """
+        Convert the number to coordinates
+        """
         cel = {}
         cell = int(cell) - 1
         for i in range(5):
             for j in range(5):
                 cel[j+i*5] = [i,j]
         return cel[cell][0],cel[cell][1]
+
+    def get_num(self,row,col):
+        """
+        Convert the coordinates to a number between 1 to 25
+        """
+        return (row*5)+col + 1
+
 
 class User(Board):
     def __init__(self, board):
@@ -71,10 +81,10 @@ class User(Board):
         Allows user to choose the spot they wish to hit
         """
         while True:
-            cell = input('Please enter a cell no 1-25 : ').upper()
+            cell = input('Please enter a cell number between 1 to 25:\n').upper()
             while cell not in [str(i) for i in range(1,26)]:
                 print("Please enter a valid cell ")
-                cell = input('Please enter a cell no 1-25 : ')
+                cell = input('Please enter a cell no 1-25 :\n')
 
             row, column = self.find_cell(cell)
             if self.print_board[row][column] != ' ':
@@ -105,9 +115,7 @@ def print_boards(u_board, c_board):
     """
     Prints two game boards to the console, one for the user and another for the computer
     """
-    print('  Player ', '\t  Computer ','\t  Reference ',)
-    print('  A B C D E ', '\t  A B C D E ',)
-    print('  _________' '\t  _________',)
+    print('    Player ', '\t  Computer ','\t    Reference \n')
     row_num = 1
     for row in range(5):
         ref = []
@@ -131,28 +139,32 @@ def main():
     print('--------------| Welcome to the Combat of Battleship |-------------')
     print('------------------------------------------------------------------\n')
     print('Test your luck against the computer in this game of battleship!\n')
-    print("Guess the positions of the 5 hidden ships on your opponent's board before the computer beats you to it!\n")
+    print("Guess the positions of the 5 hidden ships on your opponent's board before the   computer beats you to it!\n")
     
     user = User([[' ']*5 for x in range(5)])
     computer = Computer([[' ']*5 for x in range(5)])
     print_boards(u_board=user.toList(), c_board=computer.toList())
-    print(user.board_pattern)
 
     while True:
         row, column = user.guess()
         rowC, columnC = computer.choice()
-        print('You selected the coordinates: (%d, %d) \nThe computer chose: (%d, %d)'
-              % (row+1, column+1, rowC+1, columnC+1))
-        score = user.score(row, column)
-        computer.score(rowC, columnC)
+        print('\nYou selected: %d \nThe computer chose: %d \n'
+            % (user.get_num(row, column), computer.get_num(rowC, columnC)))
+        score_user = user.score(row, column)
+        score_computer = computer.score(rowC, columnC)
         print_boards(u_board=user.toList(), c_board=computer.toList())
         user.count_hits()
         computer.count_hits()
 
-        if score:
+        if score_user:
             print('Congratulations you have hit the battleship!')
         else:
-            print('Sorry, you missed!')
+            print('You missed!')
+
+        if score_computer:
+            print('The computer has hit the battleship!\n')
+        else:
+            print('The computer missed!\n')
 
         if user.count == 5 or computer.count == 5:
             if user.score_val > computer.score_val:
